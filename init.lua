edd482dea9b3c15883e00f828892c9e2c30b3651
Samo Pogačnik <samo_pogacnik@t-2.net>
2024-12-30 18:31:54 +0100

    Install first plugin neotree.nvim via lazy plugin manager

--- init.lua
+++ init.lua
@@ -11,6 +11,16 @@ vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
 require('lazy').setup({
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+}
 })
 
 -- This is the workaround for lazy.nvim resetting runtimes to VIMRUNTIME, thus
