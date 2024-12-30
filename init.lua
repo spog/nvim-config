8dd861fa363fcc78ba62dfbe0628d3c1460b9984
Samo Pogačnik <samo_pogacnik@t-2.net>
2024-12-30 18:20:38 +0100

    Install `lazy.nvim` plugin manager (no pluggins)

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
-print("Hello!")
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+})
+
+-- This is the workaround for lazy.nvim resetting runtimes to VIMRUNTIME, thus
+-- reusing the bundled parsers of my neovim DEB build!
+vim.opt.rtp:append('/usr/lib/nvim/')
