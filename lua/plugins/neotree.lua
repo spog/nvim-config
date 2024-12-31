ed02897407ac8e0844c016e1ed7cd37b471c0702
Samo Pogačnik <samo_pogacnik@t-2.net>
2024-12-31 12:56:08 +0100

    Added shortcuts to open/visualize Neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,5 +6,10 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
      "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    vim.cmd [[nnoremap \ :Neotree reveal<cr>]]
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle position=left<CR>', { noremap = true, silent = true }) -- focus file explorer
+    vim.keymap.set('n', '<leader>ngs', ':Neotree float git_status<CR>', { noremap = true, silent = true }) -- open git status window
+  end,
 }
