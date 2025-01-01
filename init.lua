5edf8f28311747a28b197ab45a197e5ade9c790c
Samo Pogačnik <samo_pogacnik@t-2.net>
2025-01-01 21:27:57 +0100

    Added 'neovim/nvim-lspconfig' plugin
    Copied from neovim-kikstart-config.
    Needed additional 'apt install python3-venv' on ubuntu to install ruff.

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ require('lazy').setup({
   require 'plugins.lualine',
   require 'plugins.treesitter',
   require 'plugins.telescope',
+  require 'plugins.lsp',
 })
 
 -- This is a workaround for lazy.nvim resetting runtime paths to
